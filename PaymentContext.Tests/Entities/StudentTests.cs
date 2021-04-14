bfc4cea7acb3c01cab83bab13ed343076766b0f9
J3eff <jeffersonvideo125@gmail.com>
2021-04-14 01:31:53 -0300

    We refined the entities with SOLID principles

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTest
+    {
+        [TestMethod]
+        public void AdicionarAssinatura()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student ("Jefferson","Brandão", "47136322861","jefferson@gmail.com");
+            student.AddSubscription(subscription);
+            
+
+        }
+    }
+}
