3d0ac3ab6408fbf51df87057d4627d086bdf0d93
J3eff <jeffersonvideo125@gmail.com>
2021-04-15 22:39:31 -0300

    Value Objects

--- PaymentContext.Domain/Entities/BoletoPayment.cs
+++ PaymentContext.Domain/Entities/BoletoPayment.cs
@@ -1,3 +1,4 @@
+using PaymentContext.Domain.ValueObjects;
 using System;
 
 namespace PaymentContext.Domain.Entities
@@ -12,8 +13,9 @@ namespace PaymentContext.Domain.Entities
             decimal total, 
             decimal totalPaid, 
             string payer, 
-            string document, 
-            string address, string email) : base(
+            Document document, 
+            Address address, 
+            Email email) : base(
                 paidDate, 
                 expireDate, 
                 total, 
--- PaymentContext.Domain/Entities/CreditCardPayment.cs
+++ PaymentContext.Domain/Entities/CreditCardPayment.cs
@@ -1,3 +1,4 @@
+using PaymentContext.Domain.ValueObjects;
 using System;
 
 namespace PaymentContext.Domain.Entities
@@ -14,8 +15,9 @@ namespace PaymentContext.Domain.Entities
             decimal total, 
             decimal totalPaid, 
             string payer, 
-            string document, 
-            string address, string email) : base(
+            Document document, 
+            Address address, 
+            Email email) : base(
                 paidDate, 
                 expireDate, 
                 total, 
